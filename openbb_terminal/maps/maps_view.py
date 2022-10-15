68b8c1651bc0b6c94c022be31e8dfec92360c66f
hjoaquim <h.joaquim@campus.fct.unl.pt>
2022-10-15 17:38:54 +0100

    added simple map pinpoint command

--- openbb_terminal/maps/maps_view.py
+++ openbb_terminal/maps/maps_view.py
@@ -4,13 +4,14 @@ import os
 import webbrowser
 import pandas as pd
 import folium
+from typing import List, Tuple
 from openbb_terminal.decorators import log_start_end
 from openbb_terminal.helper_funcs import export_data
 from openbb_terminal.rich_config import console
 
 logger = logging.getLogger(__name__)
 
-country_shapes = "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/world-countries.json"
+COUNTRY_SHAPES = "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/world-countries.json"
 
 EUROZONE_COUNTRIES = [
     "Austria",
@@ -35,23 +36,36 @@ EUROZONE_COUNTRIES = [
 ]
 
 
-def display_map(df: pd.DataFrame, legend: str, scale=None) -> None:
-    """Display map"""
-
-    m = folium.Map()  # zoom_control=False, scrollWheelZoom=False, dragging=False
+def get_folium_kwargs(
+    legend: str = None,
+    df: pd.DataFrame = None,
+    country_shapes: str = COUNTRY_SHAPES,
+    scale: list = None,
+) -> dict:
     kwargs = {
         "geo_data": country_shapes,
         "name": "choropleth",
-        "data": df,
         "columns": ["Country", "Value"],
         "key_on": "feature.properties.name",
         "fill_color": "YlOrRd",
         "fill_opacity": 0.7,
         "nan_fill_color": "white",
-        "legend_name": legend,
     }
+    if not df.empty:
+        kwargs["data"] = df
+    if legend:
+        kwargs["legend_name"] = legend
     if scale:
         kwargs["threshold_scale"] = scale
+
+    return kwargs
+
+
+def display_map(df: pd.DataFrame, legend: str, scale=None) -> None:
+    """Display map"""
+
+    m = folium.Map()  # zoom_control=False, scrollWheelZoom=False, dragging=False
+    kwargs = get_folium_kwargs(legend=legend, df=df, scale=scale)
     folium.Choropleth(**kwargs).add_to(m)
     # save folium to html
     m.save("maps.html")
@@ -130,3 +144,24 @@ def display_interest_rates(export: str = ""):
         "ir",
         df,
     )
+
+
+@log_start_end(log=logger)
+def display_map_explorer(coordinates: List[Tuple[float, float]]):
+    """
+    Display map explorer
+
+    Parameters
+    ----------
+    coordinates : List[Tuple[float, float]]
+        List of coordinates
+    """
+    m = folium.Map()
+    for coordinate in coordinates:
+        folium.Marker(location=[coordinate[0], coordinate[1]]).add_to(m)
+
+    # save folium to html
+    m.save("maps.html")
+    file_path = "maps.html"
+    webbrowser.open("file://" + os.path.realpath(file_path))
+    console.print("")
